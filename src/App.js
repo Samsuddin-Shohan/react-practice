f3b1d4a471a0597b3c2dd526837abc46590b5c13
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2021-12-09 10:51:48 +0600

    useEffect Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users,setUsers]=useState([])
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  },[]);
   return (
     <div className="App">
     <Counter></Counter>
     <Counter></Counter>
+
+    {
+      users.map(user => <User name = {user.name} email = {user.email}></User> )
+    }
       
     </div>
   );
@@ -27,6 +37,15 @@ function Counter() {
       <button onClick={handleIncrease} className='button-style'>increase</button>  
       <button onClick={handleDecrease} className='button-style'>decrease</button>  
       <h4>count is {count}</h4>
+    </div>
+  );
+}
+
+function User(props){
+  return(
+    <div className='container'>
+      <h1>Name: {props.name}</h1>
+      <h4>Email: {props.email}</h4>
     </div>
   );
 }
